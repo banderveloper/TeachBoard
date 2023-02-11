57bc2072cc4d80d10e863a751413e57803391661
banderveloper <kpssdeveloper@gmail.com>
2023-02-12 00:58:25 +0200

    feat(identity): persistence layer complete

---
+++ TeachBoard.Backend/TeachBoard.IdentityService/TeachBoard.Persistence/DatabaseInitializer.cs
@@ -0,0 +1,11 @@
+﻿namespace TeachBoard.Persistence;
+
+// db checker and initializer at server start
+// invokes at main
+public class DatabaseInitializer
+{
+    public static void Initialize(ApplicationDbContext context)
+    {
+        context.Database.EnsureCreated();
+    }
+}
