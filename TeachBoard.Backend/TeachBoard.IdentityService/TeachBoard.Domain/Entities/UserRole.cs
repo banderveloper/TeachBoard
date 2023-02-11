1cc6fb35faa66f22ab10536b8a1bf44e23673b51
banderveloper <kpssdeveloper@gmail.com>
2023-02-12 01:18:37 +0200

    feat(identity): webapi program.cs fill, injected another layers, cors, controllers and configs

--- TeachBoard.Backend/TeachBoard.IdentityService/TeachBoard.Domain/Entities/UserRole.cs
+++ TeachBoard.Backend/TeachBoard.IdentityService/TeachBoard.Domain/Entities/UserRole.cs
@@ -5,8 +5,8 @@ namespace TeachBoard.Domain.Entities;
 // Many-to-many user to roles
 public class UserRole : BaseEntity
 {
-    public Guid UserId { get; set; }
-    public Guid RoleId { get; set; }
+    public int UserId { get; set; }
+    public int RoleId { get; set; }
 
     //
     [JsonIgnore] public User? User { get; set; }
--- TeachBoard.Backend/TeachBoard.IdentityService/TeachBoard.WebApi/Program.cs
+++ TeachBoard.Backend/TeachBoard.IdentityService/TeachBoard.WebApi/Program.cs
@@ -1,6 +1,71 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Reflection;
+using System.Text.Json;
+using Microsoft.Extensions.Options;
+using TeachBoard.Application;
+using TeachBoard.Application.Configurations;
+using TeachBoard.Application.Mappings;
+using TeachBoard.Persistence;
+
+var builder = WebApplication.CreateBuilder();
+
+// Connection configuration registration
+builder.Services.Configure<ConnectionConfiguration>(builder.Configuration.GetSection("ConnectionStrings"));
+builder.Services.AddSingleton(resolver =>
+    resolver.GetRequiredService<IOptions<ConnectionConfiguration>>().Value);
+
+// DI from another layers
+builder.Services.AddApplication().AddPersistence();
+
+// CORS-policies for browser access
+builder.Services.AddCors(options =>
+{
+    // All clients (temporary)
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
+// Controller and JSON configs registration
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // lowercase for json keys
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+    });
+
+// Automapper
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(AssemblyMappingProfile).Assembly));
+});
+
+// Database initialize if it is null
+try
+{
+    var scope = builder.Services.BuildServiceProvider().CreateScope();
+    var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+    DatabaseInitializer.Initialize(context);
+}
+catch (Exception ex)
+{
+    // temporary
+    Console.WriteLine(ex);
+}
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Activate "AllowAll" CORS policy
+app.UseCors("AllowAll");
+
+app.MapGet("/hello", async context =>
+{
+    await context.Response.WriteAsync("Hello world");
+});
 
 app.Run();
