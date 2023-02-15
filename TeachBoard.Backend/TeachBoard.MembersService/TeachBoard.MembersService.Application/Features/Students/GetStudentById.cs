92d8b5933f9e4b6ea8e1a638115b9fa0061534b7
banderveloper <kpssdeveloper@gmail.com>
2023-02-15 22:46:42 +0200

    feat(members): get student by id endpoint

---
+++ TeachBoard.Backend/TeachBoard.MembersService/TeachBoard.MembersService.Application/Features/Students/GetStudentById.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using TeachBoard.MembersService.Application.Exceptions;
+using TeachBoard.MembersService.Application.Interfaces;
+using TeachBoard.MembersService.Domain.Entities;
+
+namespace TeachBoard.MembersService.Application.Features.Students;
+
+public class GetStudentByIdQuery : IRequest<Student>
+{
+    public int StudentId { get; set; }
+}
+
+
+public class GetStudentByIdQueryHandler : IRequestHandler<GetStudentByIdQuery, Student>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetStudentByIdQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Student> Handle(GetStudentByIdQuery request, CancellationToken cancellationToken)
+    {
+        var student = await _context.Students.FindAsync(request.StudentId, cancellationToken);
+
+        if (student is null)
+            throw new NotFoundException
+            {
+                Error = "student_not_found",
+                ErrorDescription = $"Student with id {request.StudentId} not found"
+            };
+
+        return student;
+    }
+}
--- TeachBoard.Backend/TeachBoard.MembersService/TeachBoard.MembersService.WebApi/Controllers/StudentController.cs
+++ TeachBoard.Backend/TeachBoard.MembersService/TeachBoard.MembersService.WebApi/Controllers/StudentController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using TeachBoard.MembersService.Application.Features.Students;
 using TeachBoard.MembersService.Application.Validation;
+using TeachBoard.MembersService.Domain.Entities;
 
 namespace TeachBoard.MembersService.WebApi.Controllers;
 
@@ -18,5 +20,14 @@ public class StudentController : ControllerBase
     {
         _mediator = mediator;
         _mapper = mapper;
+    }
+
+    [HttpGet("getbyid/{id:int}")]
+    public async Task<ActionResult<Student>> GetStudentById(int id)
+    {
+        var query = new GetStudentByIdQuery { StudentId = id };
+        var student = await _mediator.Send(query);
+
+        return Ok(student);
     }
 }
