a874190d4efe5b9f55e51a0d156090d4fa9fffe1
banderveloper <kpssdeveloper@gmail.com>
2023-02-15 23:35:38 +0200

    tests(members): get students by group id integration tests

--- TeachBoard.Backend/TeachBoard.MembersService/TeachBoard.MembersService.IntegrationTests/UserControllerIntegrationTests.cs
+++ TeachBoard.Backend/TeachBoard.MembersService/TeachBoard.MembersService.IntegrationTests/UserControllerIntegrationTests.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Http.Json;
 using TeachBoard.MembersService.Application.Exceptions;
+using TeachBoard.MembersService.Application.Features.Students;
 using TeachBoard.MembersService.Domain.Entities;
 
 namespace TeachBoard.MembersService.IntegrationTests;
@@ -34,5 +35,34 @@ public class StudentControllerIntegrationTests : IClassFixture<TestingWebAppFact
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         Assert.Equal("student_not_found", notFoundModel?.Error);
         Assert.Equal("id", notFoundModel?.ReasonField);
+    }
+
+    [Fact]
+    public async Task GetByGroupId_OnExistingGroupId_ReturnsStudents()
+    {
+        var response = await _client.GetAsync($"members/students/getbygroupid/1");
+
+        response.EnsureSuccessStatusCode();
+
+        var studentsModel = await response.Content.ReadFromJsonAsync<StudentsListModel>();
+        
+        Assert.NotNull(studentsModel);
+        Assert.Equal(2, studentsModel.Students.Count);
+
+        foreach (var student in studentsModel.Students)
+        {
+            Assert.Equal(1, student.GroupId);
+        }
+    }
+    
+    [Fact]
+    public async Task GetByGroupId_OnWrongGroupId_ReturnsNotFoundModel()
+    {
+        var response = await _client.GetAsync($"members/students/getbygroupid/50000");
+        var notFoundModel = await response.Content.ReadFromJsonAsync<NotFoundException>();
+        
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        Assert.Equal("students_not_found", notFoundModel?.Error);
+        Assert.Equal("groupId", notFoundModel?.ReasonField);
     }
 }
